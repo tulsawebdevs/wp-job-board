76e22132beab02f687dac2c6fe86d40d024571c6
Patrick Forringer <patrick@forringer.com>
2011-06-16 23:31:20 -0500

    getting extra classes added and basic post type registered.

--- job-board.php
+++ job-board.php
@@ -19,6 +19,28 @@ define( 'TWD_JB_URL', plugin_dir_url( __FILE__ ) );
 class TWD_Job_Board{
 	
 	function __construct(){
+	
+		if(!class_exists('NewPostType'))
+			require_once('classes/newposttype/new_post_type.php');
+		
+		if(!class_exists('cmb_Meta_Box')){
+			global $meta_boxes; // stupid thing you have to do
+			$meta_boxes = array();
+			
+			require_once('classes/cmb/init.php');
+		}
+			
+		NewPostType::instance()->add(array(
+			'post_type' => 'twd_job_post',
+			'post_type_name' => 'Job Post',
+			'args' => array(
+				'rewrite' => array( 'slug' => 'job' ),
+				'supports' => array( 'title', 'editor', 'excerpt' ),
+				'menu_icon' => TWD_JB_URL.'/img/luggage--plus.png'
+			)
+		))->add_taxonomy( 'jb_type', array(
+			'taxonomy_single' => 'Type'
+		));
 		
 	}
 	
